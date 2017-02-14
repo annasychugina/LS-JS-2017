6810f2053b14286bc593f5915a5cea496d493e48
sychuginaanna <sychuginaanna@ya.ru>
2017-02-14 23:39:03 +0300

    add 8 task

--- loftschool-example/hw3/src/index.js
+++ loftschool-example/hw3/src/index.js
@@ -122,16 +122,8 @@ function upperProps(obj) {
 function slice(array, from, to = array.length) {
     var result = [];
 
-    // from = (typeof from === 'undefined' || from < -array.length) ? 0 : from;
-    // from = (from < 0) ? array.length + from : from;
-
-    if (!from || from < -array.length) {
-        from = 0;
-    }
-
-    if (from < 0) {
-        from = array.length + from;
-    }
+    from = (typeof from === 'undefined' || from < -array.length) ? 0 : from;
+    from = (from < 0) ? array.length + from : from;
 
     if (to > array.length) {
         to = array.length;
