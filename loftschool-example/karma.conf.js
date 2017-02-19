818c5c4aa9e5595cdb0c688b96c3ff6cacd53ffc
sychuginaanna <sychuginaanna@ya.ru>
2017-02-19 17:25:56 +0300

    change package.json and karma.conf

--- loftschool-example/karma.conf.js
+++ loftschool-example/karma.conf.js
@@ -3,10 +3,10 @@ module.exports = function(config) {
         basePath: '',
         frameworks: ['mocha'],
         files: [
-            './hw4/test/**/*.js'
+            './hw5/test/**/*.js'
         ],
         preprocessors: {
-            './hw4/test/**/*.js': ['webpack', 'sourcemap'],
+            './hw5/test/**/*.js': ['webpack', 'sourcemap'],
         },
         webpack: require('./webpack.config.test'),
         webpackMiddleware: {
