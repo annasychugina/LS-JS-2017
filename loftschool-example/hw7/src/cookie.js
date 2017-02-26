2ef732dbf67d86f2b2f9e153faa4e46c926ffce2
sychuginaanna <sychuginaanna@ya.ru>
2017-02-26 09:07:01 +0300

    finish hw7

--- loftschool-example/hw7/src/cookie.js
+++ loftschool-example/hw7/src/cookie.js
@@ -53,11 +53,7 @@ let listTable = homeworkContainer.querySelector('#list-table tbody');
  * @return {boolean}
  */
 function isMatching(full, chunk) {
-    if (full.toUpperCase().indexOf(chunk.toUpperCase()) === -1) {
-
-        return false;
-    }
-    return true;
+    return Boolean(full.toLowerCase().indexOf(chunk.toLowerCase()) + 1);
 }
 
 /**
@@ -67,46 +63,28 @@ function isMatching(full, chunk) {
  * @param value - значение cookie
  */
 
-// создание куки
 function createCookie(name, value) {
     document.cookie = `${name}=${value}`;
 }
 
-// удаление куки
-function deleteCookie(name) {
-    document.cookie = `${name}=; expires=${new Date(0)}`;
-}
-
-// создание куки в таблице
 function createCookieTr(name, value) {
-    document.cookie = `${name}=${value}`;
-    // строка
     let row = document.createElement('tr');
-    // кнопка удаления
     let removeButton = document.createElement('button');
-    // получаем табличку
     let table = document.getElementById('list-table');
 
-    // от нуля до последнего элемента таблицы
     for (let i = 0; i < table.rows[0].cells.length; i++) {
-        // создаем ячейку
         let cell = document.createElement('th');
-        // вставляем ячейку в строку
+
         row.appendChild(cell);
     }
-    // name
     row.children[0].textContent = name;
-    // value
     row.children[1].textContent = value;
-    // removeButton
     row.children[2].appendChild(removeButton);
     removeButton.textContent = 'Удалить';
-    removeButton.classList.add("button__remove");
-    // вставляем строку в табличу
+    removeButton.classList.add('button__remove');
     listTable.appendChild(row);
 }
 
-// получение куки
 function getCookies() {
     return document.cookie
         .split('; ')
@@ -119,74 +97,49 @@ function getCookies() {
         }, {});
 }
 
-
-// поиск куки
-function findCookie(name) {
+function updateTable(name, value) {
     for (let row of listTable.children) {
         if (row.firstElementChild.innerText === name) {
-            return row.firstElementChild;
+            let nameCell = row.firstElementChild;
+
+            nameCell.nextElementSibling.textContent = value;
         }
     }
-
-    return null;
-
+    createCookieTr(name, value);
 }
 
-// фильтрация куки
+function filterCookies() {
+    let value = filterNameInput.value.trim();
+    let cookies = getCookies();
 
-
-
-// добавление в таблицу куки
-function updateTable(name, value) {
-    let nameCell = findCookie(name);
-
-    if (nameCell) {
-        nameCell.nextElementSibling.textContent = value;
-    } else {
-        createCookieTr(name, value);
+    listTable.innerHTML = '';
+    for (let key in cookies) {
+        if (isMatching(key, value) || isMatching(cookies[key], value)) {
+            createCookieTr(key, cookies[key]);
+        }
     }
-
 }
 
-
-
-function deleteCookieTr(nameTd) {
-    let tr = nameTd.closest('tr');
-
-    deleteCookie(nameTd.innerText);
-    listTable.removeChild(tr);
-}
-
-
-
-
-// filterNameInput.addEventListener('keyup');
-
-addButton.addEventListener('click', () => {
+function addCookies() {
     let name = addNameInput.value;
     let value = addValueInput.value;
     let cookies = getCookies();
 
     createCookie(name, value);
-
-    if (name in cookies) {
-        updateTable(name, value)
-    } else {
-        createCookieTr(name, value);
-    }
-
-});
-
-// удаление куки
-listTable.addEventListener('click', (e) => {
-   if (e.target.className === 'button__remove') {
-       let name = e.target.closest('tr').children[0].textContent;
-       let row = e.target.closest('tr');
-
-       deleteCookie(name);
-       deleteCookieTr(row);
-   }
+    (name in cookies) ? updateTable(name, value) : createCookieTr(name, value);
+    filterCookies();
 }
 
+function removeCookies(e) {
+    if (e.target.className === 'button__remove') {
+        let name = e.target.closest('tr').children[0].textContent;
+        let row = e.target.closest('tr');
 
-);
+        document.cookie = `${name}=; expires=${new Date(0)}`;
+        listTable.removeChild(row);
+    }
+}
+
+filterNameInput.addEventListener('keyup', filterCookies);
+addButton.addEventListener('click', addCookies);
+listTable.addEventListener('click', removeCookies);
