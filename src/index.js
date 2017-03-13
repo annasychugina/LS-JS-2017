9b015e2767443007283625edee24147f55ce9eb2
sychuginaanna <sychuginaanna@ya.ru>
2017-03-13 13:03:23 +0300

    верстка блока

---
+++ src/index.js
@@ -0,0 +1,29 @@
+require('./styles/myfriend.css');
+
+
+function login() {
+    return new Promise((resolve, reject) => {
+        VK.init({
+            apiId: 5918600
+        });
+        VK.Auth.login(function(result) {
+            if (result.status == 'connected') {
+                resolve();
+            } else {
+                reject();
+            }
+        });
+    });
+}
+
+function callAPI(method, params) {
+    return new Promise((resolve, reject) => {
+        VK.api(method, params, function(result) {
+            if (result.error) {
+                reject();
+            } else {
+                resolve(result.response);
+            }
+        });
+    });
+}
